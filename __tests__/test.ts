cb34535e3cdce1a17e3c4368e54bcb053deddb6f
Wil Lee <kourge@gmail.com>
2017-03-22 17:15:38 -0700

    Expand baseline tests

--- __tests__/test.ts
+++ __tests__/test.ts
@@ -1,7 +1,11 @@
 import { Enum } from "../src/index";
 
 describe("Enum", () => {
-    it("should return an object with values equal to keys", () => {
+    it("returns an object with values equal to keys given multiple strings", () => {
         expect(Enum("BLACK", "WHITE")).toEqual({ BLACK: "BLACK", WHITE: "WHITE" });
+    });
+
+    it("returns an object with values equal to keys given a single string", () => {
+        expect(Enum("BLACK")).toEqual({ BLACK: "BLACK" });
     });
 });
